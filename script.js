8ed788bdda1f5557a94461cb79db49f4314ad683
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-14 12:39:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,19 @@
-//your JS code here. If required.
-const codes = document.querySelectorAll('.code');
+const inputs = document.querySelectorAll(".code");
 
-// Auto focus first input
-codes[0].focus();
+inputs[0].focus();
 
-codes.forEach((code, index) => {
+inputs.forEach((input, index) => {
 
-  code.addEventListener('keydown', (e) => {
-
-    // If number is typed
-    if (e.key >= 0 && e.key <= 9) {
-      code.value = '';
-      setTimeout(() => {
-        if (index < codes.length - 1) {
-          codes[index + 1].focus();
-        }
-      }, 10);
+  input.addEventListener("input", () => {
+    if (input.value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
     }
+  });
 
-    // Backspace behavior
-    else if (e.key === 'Backspace') {
-
-      if (code.value === '' && index > 0) {
-        codes[index - 1].focus();
-      } else {
-        code.value = '';
-      }
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && !input.value && index > 0) {
+      inputs[index - 1].focus();
     }
-
   });
 
 });
